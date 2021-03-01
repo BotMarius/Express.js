7d4d01a7ffb7678768cef5d3d82af79089bd18c7
Marius <marius.etourneau@outlook.fr>
2021-03-01 10:15:28 +0100

    fini

--- serveur.js
+++ serveur.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -15,17 +29,31 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
+    res.render('pages/login.ejs');
+});
 
+app.post('/auth/login', function(req, res) {
+    let logged = false;
     const username = req.body.username;
     const password = req.body.password;
 
-    res.render('pages/login.ejs', {username, password});
+    if(username === "admin" && password === "admin") {
+        logged = true;
+    }
+
+    res.render('pages/dashboard.ejs', {username, password, logged});
 });
 
 app.get('/dl', function(req, res){
     const file = `${__dirname}/public/img/pepo.jpg`;
     res.download(file);
 });
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
 
 app.get('/chapitre/:nombre', function(req, res){
     res.render('pages/chapitre.ejs', {
